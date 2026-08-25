122b123233f8a17f34ac8e1781f92247caaff5ca
zlfn <ung@zlfn.space>
2026-08-25 18:06:22 +0900

    [Z80] Emit repeated non-zero bytes as .db for sdas
    Fixes #32

--- llvm/include/llvm/MC/MCAsmInfo.h
+++ llvm/include/llvm/MC/MCAsmInfo.h
@@ -114,6 +114,8 @@ protected:
   // True if using the HLASM dialect on z/OS.
   bool IsHLASM = false;
 
+  bool IsSDCC = false;
+
   /// This is the maximum possible length of an instruction, which is needed to
   /// compute the size of an inline asm.  Defaults to 4.
   unsigned MaxInstLength = 4;
@@ -518,6 +520,7 @@ public:
 
   bool isAIX() const { return IsAIX; }
   bool isHLASM() const { return IsHLASM; }
+  bool isSDCC() const { return IsSDCC; }
   bool isMachO() const { return HasSubsectionsViaSymbols; }
   bool hasCOFFAssociativeComdats() const { return HasCOFFAssociativeComdats; }
   bool hasCOFFComdatConstants() const { return HasCOFFComdatConstants; }
